ee514b06cfc7b2ed7f5847a180b836ff4723f8ef
Moaz707 <32021046+Moaz707@users.noreply.github.com>
2018-01-12 11:54:10 -0500

    init app

---
+++ app/src/test/java/com/muaadh_melhi_nycdeveloper/myapplication/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.muaadh_melhi_nycdeveloper.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
